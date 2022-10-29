df9ab36412bb111558b9dafcbf1e5e852decc391
rstap1 <115902899+rstap1@users.noreply.github.com>
2022-10-29 14:54:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # this is the main file for the project - put your code here
 
+class shapes:
+    x = 0
+    y = 0
+
+    shapes = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]], # line
+        [4, 5, 8, 9], # square
+        [[2, 6, 5, 9], [1, 5, 6, 10]], # vertical z
+        [[2, 3, 5, 6], [1, 2, 6, 7]], # z shape
+        [[4, 5, 6, 9], [1, 4, 5, 6], [1, 5, 9, 6], [1, 4, 5, 9]], # T shape
+        [[0, 4, 8, 9], [1, 5, 9, 8], [4, 0, 1, 2], [6, 2, 1, 0]] # L shape
+    ]
+
 print("hello world")
